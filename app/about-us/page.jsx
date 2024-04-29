53a5febd2e0cb63cb86414fb8f16da103e7e1d67
petranih <gokilfitra528@gmail.com>
2024-04-29 21:01:37 +0700

    first

---
+++ app/about-us/page.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function AboutUsPage () {
+    return (
+        <>
+    <h1 className="mb-14">hello men</h1>
+    <Link href="/login">login link</Link>
+    <a href="/login" className="bg-slate-700 border-yellow-800 ">login</a>
+    </>
+)    
+}
---
+++ app/login/page.jsx
@@ -0,0 +1,9 @@
+export default function LoginPage () {
+    return (
+        <form action="" className="bg-indigo-600 flex-shrink">
+            <input type="text" className="border-solid mr-5"/>
+            <input type="text" />
+            <button className="border-r-black bg-blue-800">submid</button>
+        </form>
+    )    
+}
